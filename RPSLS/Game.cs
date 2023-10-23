92fe7a0af3425dbf7d3187315c5a8236ad9675c2
Uriel Martinez <martinezuriel651@gmail.com>
2023-10-23 14:09:37 -0500

    Uploading starter code

---
+++ RPSLS/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    internal class Game
+    {
+        //Member Variabes (HAS A)
+        public Player playerOne;
+        public Player playerTwo;
+
+        //Constructor
+        public Game()
+        {
+
+        }
+
+        //Member Methods (CAN DO)
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+        }
+
+        public int ChooseNumberOfHumanPlayers()
+        {
+            return 0;
+        }
+
+        public void CreatePlayerObjects(int numberOfHumanPlayers)
+        {
+
+        }
+
+        public void CompareGestures()
+        {
+
+        }
+
+        public void DisplayGameWinner()
+        {
+
+        }
+
+        public void RunGame()
+        {
+            WelcomeMessage();
+        }
+    }
+}
